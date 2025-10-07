5d2d641ba67024370616ebf18d25aadfaf69a9f1
Bonaventure CJ <ugwubonaventure@gmail.com>
2025-10-07 16:22:58 +0100

    refactor: use absolute import statements

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,13 +1,13 @@
 // src/data/projects.js
 
 // Import all project images from the assets folder.
-import portfolio from '../assets/images/projects/portfolio.png';
-import mealRecipeAppImage from '../assets/images/projects/yummyrecipeshub.png';
-import qrCodeGeneratorImage from '../assets/images/projects/qr-code-generator.png';
-import githubProfileFinderImage from '../assets/images/projects/github-profile-finder.png';
-import movieSearchAppImage from '../assets/images/projects/globeflix.png';
-import contactManagerImage from '../assets/images/projects/contact-manager.png';
-import dashboardImage from '../assets/images/projects/dashboard.png';
+import portfolio from 'assets/images/projects/portfolio.png';
+import mealRecipeAppImage from 'assets/images/projects/yummyrecipeshub.png';
+import qrCodeGeneratorImage from 'assets/images/projects/qr-code-generator.png';
+import githubProfileFinderImage from 'assets/images/projects/github-profile-finder.png';
+import movieSearchAppImage from 'assets/images/projects/globeflix.png';
+import contactManagerImage from 'assets/images/projects/contact-manager.png';
+import dashboardImage from 'assets/images/projects/dashboard.png';
 
 import generateId from 'utils/helpers';
 
