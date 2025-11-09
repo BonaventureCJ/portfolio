89a8376333474c5ad39fdc3936a8c30b1c32a077
Bonaventure CJ <ugwubonaventure@gmail.com>
2025-11-09 20:46:02 +0100

    chore: Update screenshot and remove dev code
    Update the outdated project screenshot on the portfolio to reflect
    recent UI changes.
    
    This commit also removes unused development code from the `Home.jsx`
    page for a cleaner, production-ready codebase.
    
    - Replaced outdated screenshot on the Projects page with
     a current version.
    - Cleaned up unused, commented-out component imports.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,21 +5,14 @@ import SkillsPreview from 'components/previews/SkillsPreview/SkillsPreview';
 import ProjectsPreview from 'components/previews/ProjectsPreview/ProjectsPreview';
 import ContactPreview from 'components/previews/ContactPreview/ContactPreview';
 
-/* DEV MODE TEST IMPORTS FOR NEW COMPONENTS */
-//import ExampleButton from 'components/Buttons/ExampleButton';
-
 
 const Home = () => {
   return (
     <>
-      <h1>Welcome to My Portfolio 3.0.0 in the Name of Jesus Christ!</h1>
-      {/* DEV MODE TEST RENDERING FOR NEW COMPONENTS */}
-      {/* <ExampleButton /> */}
-
       {/* Home Page Sections */}
       <Hero />
       <AboutPreview />
-      {/* Use the reusable SkillsPreview component also used in About Page*/}
+      {/* Reusable SkillsPreview component also used in About Page*/}
       <SkillsPreview />
       <ProjectsPreview />
       <ContactPreview />
